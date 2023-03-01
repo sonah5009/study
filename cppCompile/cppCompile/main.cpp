f3360a0dce0ae8c64bd6f1f96371cb6ff0ec2e0d
sonah5009 <sonah5009@gmail.com>
2023-03-01 15:09:47 +0900

    Upload: react-app

---
+++ cppCompile/cppCompile/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cppCompile
+//
+//  Created by 최선아 on 2022/11/02.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
